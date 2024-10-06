9f5d13a572097c13b8dd204d768e8b614ffd301c
Reuben <grerlich@gmail.com>
2024-10-05 20:47:49 -0400

    add dogs

--- server.js
+++ server.js
@@ -1,9 +1,52 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
 const express = require('express')
 const mongoose = require('mongoose')
 
+const app = express();
+app.use(express.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => {
-  res.send('Hello There!');
+const Dog = require("./models/dog.js");
+
+mongoose.connect(process.env.MONGODB_URI); 
+mongoose.connection.on('connected', () => {
+  console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+})
+
+
+app.get('/', async (req, res) => {
+  res.render("index.ejs");
 });
+
+app.get("/dogs", async (req, res) => {
+  const allDogs = await Dog.find();
+  res.render("dogs/index.ejs", { dogs: allDogs })
+});
+
+app.get("/dogs/new", (req, res) => {
+  res.render("dogs/new.ejs")
+});
+
+app.post("/dogs", async (req, res) => {
+  if (req.body.isCute === "on") {
+    req.body.isCute = true;
+  } else {
+    req.body.isCute = false;
+  }
+  await Dog.create(req.body);
+  res.redirect("/dogs/new");
+});
+
+app.get('/dogs', (req, res) => {
+  res.render("index.ejs");
+})
+
+
+app.post("/dogs", async (req, res) => {
+  console.log(req.body);
+  res.redirect("/dogs/new");
+});
+
 
 app.listen(3000);
